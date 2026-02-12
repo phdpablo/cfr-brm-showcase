8a4ad99eafa9816c965da53ae9062053811c785a
Pablo Rogers <86000533+phdpablo@users.noreply.github.com>
2026-02-12 16:14:58 -0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANTE: Isso deve corresponder ao nome do seu repositório no GitHub
-  base: '/cfr-brm-showcase/',
+  base: '/cfa-brm-showcase/',
 })
