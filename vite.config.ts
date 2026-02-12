33272fa316d6d53599fd9e2ebfb328da122bf248
Pablo Rogers <86000533+phdpablo@users.noreply.github.com>
2026-02-12 16:15:39 -0300

    Clean up comments in vite.config.ts
    Removed unnecessary comments from the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Isso deve corresponder ao nome do seu repositório no GitHub
   base: '/cfa-brm-showcase/',
 })
